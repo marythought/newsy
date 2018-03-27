aa0ac6de06db5b7db6dc054dee94ae9de3593640
Mary Dickson Diaz <mdickson@gmail.com>
2018-03-26 20:17:20 -0700

    add nyt articles

---
+++ README.md
@@ -0,0 +1,9 @@
+TODO:
+- fix datetime on articles (each API uses a diff format)
+- save aggregated articles in a document-oriented database (MongoDB or ?)
+- return articles from a DB query instead of directly from the API calls
+- add cacheing, and/or some way of checking when APIs were last queried
+- sort by time published (most recent to oldest, the only way to sort things, take notes twitter and instagram)
+
+Resources:
+- https://www.mongodb.com/blog/post/building-your-first-application-mongodb-creating-rest-api-using-mean-stack-part-1
--- newsService/service.go
+++ newsService/service.go
@@ -29,21 +29,10 @@ type TCArticles struct {
 
 type TCArticle struct {
 	Source source     `json:"source"`
+	Author string     `json:"author"`
 	Title  string     `json:"title"`
 	URL    string     `json:"url"`
 	Time   *time.Time `json:"publishedAt"`
-
-	// "source": {
-	// "id": "techcrunch",
-	// "name": "TechCrunch"
-	// },
-	// "author": "Matt Burns",
-	// "title": "BMW and Lexus look to car subscriptions",
-	// "description": "More automakers will soon offer vehicles through subscription services. Lexus today announced its upcoming UX crossover would be available through one and Bloomberg published a report today stating BMW is about to announce a subscription pilot. These automake…",
-	// "url": "https://techcrunch.com/2018/03/26/bmw-and-lexus-look-to-car-subscriptions/",
-	// "urlToImage": "https://techcrunch.com/wp-content/uploads/2018/03/lexus-ux.jpg?w=649",
-	// "publishedAt": "2018-03-26T23:20:16Z"
-	// },
 }
 
 type source struct {
@@ -58,10 +47,30 @@ func (a TCArticle) toArticle() (art Article) {
 	return art
 }
 
+type NYTArticles struct {
+	Articles []NYTArticle `json:"results"`
+}
+
+type NYTArticle struct {
+	URL   string `json:"url"`
+	Title string `json:"title"`
+	// Time   *time.Time `json:"published_date"`
+	Source string `json:"source"`
+}
+
+func (a NYTArticle) toArticle() (art Article) {
+	art.ID = 0
+	art.Source = a.Source
+	art.Title = a.Title
+	art.URL = a.URL
+	return art
+}
+
 // GetArticles is a function that calls Hackernews, TechCrunch and NYT APIs to get, aggregate and return top articles
 func GetArticles(w http.ResponseWriter, r *http.Request) {
 	articles := getHackerNewsTopStories()
-	articles = append(articles, getTechCrunch()...)
+	articles = append(articles, getTechCrunchArticles()...)
+	articles = append(articles, getNYTArticles()...)
 	json.NewEncoder(w).Encode(articles)
 }
 
@@ -74,13 +83,6 @@ func getHackerNewsTopStories() []Article {
 	return articles
 }
 
-// GET https://hacker-news.firebaseio.com/v0/item/{ID}.json
-// TODO: fix time
-//  {"id":8863,
-// 	"score":104,
-// 	"time":1175714200,
-// 	"title":"My YC app: Dropbox - Throw away your USB drive",
-// 	"url":"http://www.getdropbox.com/u/2/screencast.html"}
 func getHNArticle(id string) Article {
 	art := Article{Source: "HackerNews"}
 	resp, err := http.Get("https://hacker-news.firebaseio.com/v0/item/" + id + ".json")
@@ -89,6 +91,9 @@ func getHNArticle(id string) Article {
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
 	err = json.Unmarshal(body, &art)
 	if err != nil {
 		fmt.Println(err)
@@ -97,21 +102,25 @@ func getHNArticle(id string) Article {
 	return art
 }
 
-// /v0/topstories -- returns an array of IDs (up to 500)
 func getHackerNewsTopIDs() []int {
 	var ids []int
 	resp, err := http.Get("https://hacker-news.firebaseio.com/v0/topstories.json")
 	if err != nil {
-		// handle error
+		fmt.Println(err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
 	err = json.Unmarshal(body, &ids)
+	if err != nil {
+		fmt.Println(err)
+	}
 	return ids[:10]
 }
 
-// GET
-func getTechCrunch() []Article {
+func getTechCrunchArticles() []Article {
 	articles := []Article{}
 	err := godotenv.Load()
 	if err != nil {
@@ -126,50 +135,40 @@ func getTechCrunch() []Article {
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
 	err = json.Unmarshal(body, &m)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(m)
-
 	for _, a := range m.Articles {
 		articles = append(articles, a.toArticle())
 	}
-
 	return articles
-	// returns:
-	// {
-	// "status": "ok",
-	// "totalResults": 10,
-	// -"articles": [
-	// -{
-	// -"source": {
-	// "id": "techcrunch",
-	// "name": "TechCrunch"
-	// },
-	// "author": "Matt Burns",
-	// "title": "BMW and Lexus look to car subscriptions",
-	// "description": "More automakers will soon offer vehicles through subscription services. Lexus today announced its upcoming UX crossover would be available through one and Bloomberg published a report today stating BMW is about to announce a subscription pilot. These automake…",
-	// "url": "https://techcrunch.com/2018/03/26/bmw-and-lexus-look-to-car-subscriptions/",
-	// "urlToImage": "https://techcrunch.com/wp-content/uploads/2018/03/lexus-ux.jpg?w=649",
-	// "publishedAt": "2018-03-26T23:20:16Z"
-	// },
-	// -{
-	// -"source": {
-	// "id": "techcrunch",
-	// "name": "TechCrunch"
-	// },
-	// "author": "Katie Roof",
-	// "title": "Microsoft surges 8% after Morgan Stanley says it will reach $1 trillion market cap",
-	// "description": "The Dow surged 669 points on Monday after trade tensions eased. Tech stocks like Amazon and Apple saw gains, but the biggest winner of all was Microsoft . The Seattle tech giant, which is a Dow 30 company, benefitted not only from the solid stock market day, …",
-	// "url": "https://techcrunch.com/2018/03/26/microsoft-surges-8-after-morgan-stanley-says-it-will-reach-1-trillion-market-cap/",
-	// "urlToImage": "https://techcrunch.com/wp-content/uploads/2017/07/gettyimages-496394442.jpg?w=643",
-	// "publishedAt": "2018-03-26T22:11:38Z"
-	// },
-	// ]
-	// }
 }
 
-func getNYT() {
+func getNYTArticles() []Article {
+	articles := []Article{}
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	token := os.Getenv("NYT_TOKEN")
 
+	m := NYTArticles{}
+	resp, err := http.Get("https://api.nytimes.com/svc/mostpopular/v2/mostviewed/Technology/7.json?api-key=" + token)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	err = json.Unmarshal(body, &m)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, a := range m.Articles {
+		articles = append(articles, a.toArticle())
+	}
+	return articles
 }
